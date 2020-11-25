aa23ad2e83ac89d7c831d2600910ccff1c1cebe8
Sosefo <ludovic.gillot1@etu.univ-lorraine.fr>
2020-11-25 11:08:01 +0100

    generate components

--- mini-site/src/app/app.module.ts
+++ mini-site/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { JeuxComponent } from './jeux/jeux.component';
+import { ConsolesComponent } from './consoles/consoles.component';
+import { RessourcesComponent } from './ressources/ressources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    JeuxComponent,
+    ConsolesComponent,
+    RessourcesComponent
   ],
   imports: [
     BrowserModule
