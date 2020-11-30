e288c5817120eadd9ce9e590f7537749faa37218
Jorane <jorane.schuster1@etu.univ-lorraine.fr>
2020-11-30 20:53:17 +0100

    add peripheriques text + fix carousel bug ( but it'sHORRIBLE)

--- mini-site/src/app/peripheriques/peripheriques.component.ts
+++ mini-site/src/app/peripheriques/peripheriques.component.ts
@@ -1,16 +1,26 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as $ from 'jquery';
+import { CarouselComponent } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-peripheriques',
   templateUrl: './peripheriques.component.html',
   styleUrls: ['./peripheriques.component.css']
 })
-export class PeripheriquesComponent implements OnInit{
+export class PeripheriquesComponent implements OnInit,AfterViewInit{
 
 
   constructor() {
   }
+
+  @ViewChild('slide1')
+  slide1: CarouselComponent;
+  @ViewChild('slide2')
+  slide2: CarouselComponent;
+  @ViewChild('slide3')
+  slide3: CarouselComponent;
+  @ViewChild('slide4')
+  slide4: CarouselComponent;
 
   activeSlide = 0;
 
@@ -18,23 +28,42 @@ export class PeripheriquesComponent implements OnInit{
 
   ngOnInit(): void {
 
+
+  }
+
+  ngAfterViewInit(): void {
+    this.slide2.pause();
+    this.slide3.pause();
+    this.slide4.pause();
+
   }
 
   changeActivePeripherique(peripherique: number): void{
-    console.log(peripherique);
     this.activePeripherique = peripherique;
-  }
 
-  nextSlide(): void{
-    if (this.activeSlide === 1){
-      this.activeSlide = 0;
+    if (peripherique === 1){
+      this.slide1.play();
+      this.slide2.pause();
+      this.slide3.pause();
+      this.slide4.pause();
     }
-    else{
-      this.activeSlide++;
+    else if (peripherique === 2){
+      this.slide2.play();
+      this.slide1.pause();
+      this.slide3.pause();
+      this.slide4.pause();
+    }
+    else if (peripherique === 3){
+      this.slide3.play();
+      this.slide1.pause();
+      this.slide2.pause();
+      this.slide4.pause();
+    }
+    else {
+      this.slide4.play();
+      this.slide1.pause();
+      this.slide2.pause();
+      this.slide3.pause();
     }
   }
-
-
-
-
 }
